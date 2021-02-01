411343b2bccfa32aef4e2680ade3792bc4f795c4
wulan17 <galihgustip@gmail.com>
2021-02-02 01:44:28 +0700

    ocs-url
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export repo="realvnc-vnc-viewer"
+export repo="ocs-url"
 pacman -Syu
 pacman -S --noconfirm --needed base-devel git curl wget ccache git tar xz zstd python python-pip
 # use all possible cores for subsequent package builds
