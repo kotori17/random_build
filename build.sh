d4fdfb51d2ea183165fe7977a6ec9c7d0cae3476
wulan17 <galihgustip@gmail.com>
2021-02-02 00:20:57 +0700

    scrcpy
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export repo="snapd"
+export repo="scrcpy"
 pacman -Syu
 pacman -S --noconfirm --needed base-devel git curl wget ccache git tar xz zstd python python-pip
 # use all possible cores for subsequent package builds
