a4b9446844836e2cc2a8f65a158bee8dd4bf4f8a
wulan17 <galihgustip@gmail.com>
2021-02-02 01:08:41 +0700

    processing
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export repo="realvnc-vnc-viewer"
+export repo="processing"
 pacman -Syu
 pacman -S --noconfirm --needed base-devel git curl wget ccache git tar xz zstd python python-pip
 # use all possible cores for subsequent package builds
@@ -8,12 +8,13 @@ sed -i 's,!ccache,ccache,g' /etc/makepkg.conf
 useradd --create-home wulan17
 usermod --append --groups wheel wulan17
 echo "wulan17 ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers
+cp -r processing /home/wulan17/processing
 chown -R wulan17 /home/wulan17
 cd /home/wulan17
 su wulan17 -c "git clone https://wulan17:$token@github.com/wulan17/credentials.git -b up upload" 
 su wulan17 -c 'pip install telethon tgcrypto'
 su wulan17 -c 'pip install -r upload/requirements.txt'
-su wulan17 -c "git clone https://aur.archlinux.org/$repo.git" 
+#su wulan17 -c "git clone https://aur.archlinux.org/$repo.git" 
 cd "$repo"
 su wulan17 -c 'makepkg -s --noconfirm'
 ls -lah
