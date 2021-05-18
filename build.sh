11a9d1d308a37364405cc155516c5c77bdb1c229
wulan17 <galihgustip@gmail.com>
2021-05-18 14:36:10 +0700

    5.12.21
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- build.sh
+++ build.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 
 pacman -Sy
-pacman -S --noconfirm wget
+pacman -S --noconfirm wget jdk15-openjdk git python python-pip
 # use all possible cores for subsequent package builds
 sed -i 's,#MAKEFLAGS="-j2",MAKEFLAGS="-j$(nproc)",g' /etc/makepkg.conf
 sed -i 's,!ccache,ccache,g' /etc/makepkg.conf
-cp patch-for-spflashtool.patch Allow-Disable-MSR-Lockdown.patch remove-plus-char-from-localversion.patch PKGBUILD .SRCINFO linux-zen-wulan17.conf linux-zen-wulan17.install linux-zen-wulan17.preset localversion.patch /home/wulan17/
-cp makepkg.conf /etc/
-chmod 0644 /etc/makepkg.conf
+cp jdk/* /home/wulan17/
+cp -r upload /home/wulan17
 chown -R wulan17 /home/wulan17
-cd /home/wulan17
-su wulan17 -c 'mkdir src'
-su wulan17 -c 'mkdir src/build'
-su wulan17 -c 'mkdir pkg'
-su wulan17 -c 'wget -O src/build/.config https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/linux-zen/trunk/config'
-cd src/build && patch -Np1 -i /home/wulan17/localversion.patch
-su wulan17 -c 'chown -R wulan17 /home/wulan17/src/build'
-cd /home/wulan17
+cd /home/wulan17/
 export BUILD_START=$(date +"%s")
+cd /home/wulan17
 su wulan17 -c 'makepkg -s --noconfirm'
 export BUILD_END=$(date +"%s")
-if [[ ! -z $(ls linux-zen-git-5.11*.pkg.* | cut -d "/" -f 5) ]]; then
-	export filename="$(ls linux-zen-git-5.11*.pkg.*)"
-	export headername="$(ls linux-zen-git-headers-5.11*.pkg.*)"
-	curl -F secret="$ci_secret" -F document=@"$(pwd)"/"$filename" -F caption="Build success\nFilename: $filename" https://ci.wulan17.my.id/sendDocument
-	curl -F secret="$ci_secret" -F document=@"$(pwd)"/"$headername" -F caption="Build success\nFilename: $headername" https://ci.wulan17.my.id/sendDocument
+if [[ ! -z $(ls *jdk*.pkg.* | cut -d "/" -f 5) ]]; then
+	export filename="$(ls *jdk*.pkg.*)"
+	export headername="$(ls *jre*.pkg.*)"
+	#curl -F secret="$ci_secret" -F document=@"$(pwd)"/"$filename" -F caption="Build success\nFilename: $filename" https://ci.wulan17.my.id/sendDocument
+	#curl -F secret="$ci_secret" -F document=@"$(pwd)"/"$headername" -F caption="Build success\nFilename: $headername" https://ci.wulan17.my.id/sendDocument
+	pacman -Syu --noconfirm
+	su wulan17 -c 'pip install -r /home/wulan17/upload/requirements.txt'
+	su wulan17 -c "bash /home/wulan17/upload/up.sh /home/wulan17/*jdk*.pkg.tar*"
+	su wulan17 -c "bash /home/wulan17/upload/up.sh /home/wulan17/*jre*.pkg.tar*"
 	exit 0
 else
 	exit 1
